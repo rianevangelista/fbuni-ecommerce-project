2d5c84f5bc2f81ba301b37c5ff97fec2f69b6f5a
Rian Evangelista <rianevangelista@hotmail.com>
2022-11-30 18:16:26 -0300

    CRUD - COMMIT 0.1
    30/11/2022 - Cadastro de produtos e leitura

---
+++ frontend/src/app/views/product-crud/product-crud.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-product-crud',
+  templateUrl: './product-crud.component.html',
+  styleUrls: ['./product-crud.component.css']
+})
+export class ProductCrudComponent {
+
+  constructor(private router: Router) {
+
+  }
+
+  navigateToProductCreate(): void {
+    this.router.navigate(['/products/create'])
+  }
+}
